cd7ae25a3fb5c468ffdadbcbe1911e955f8e4b9c
Amit <amitsivansh2001@gmail.com>
2023-06-23 09:01:22 +0530

    Day-01

---
+++ servefood/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Home from './screen/Home';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from './screen/Login';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route exact path="/login" element={<Login/>}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ servefood/src/screen/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <Footer />
+    </>
+  );
+}
