c3f0c797ce7b6a43086fd663ee17e3b21c11aeb7
Amit <amitsivansh2001@gmail.com>
2023-06-24 12:31:47 +0530

    Day-02

--- servefood/src/components/NavBar.js
+++ servefood/src/components/NavBar.js
@@ -3,10 +3,10 @@ import {Link} from 'react-router-dom'
 export default function NavBar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success " style={{filter: "brightness(150%"}}>
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            Navbar
+          <Link className="navbar-brand fs-1 fst-italic" to="/">
+            GoFood
           </Link>
           <button
             className="navbar-toggler"
