424e5d6e15eb6cb4b56039e40be8f9e15430dc6d
Amit <amitsivansh2001@gmail.com>
2023-07-23 00:58:51 +0530

    23rd July

--- servefood/Backend/Routes/CreateUser.js
+++ servefood/Backend/Routes/CreateUser.js
@@ -1,8 +1,16 @@
 const exp = require('express')
 const router = exp.Router()
 const user = require('../model/user')
-
-router.post('/createuser', async (req,res)=>{
+const { body, validationResult } = require('express-validator');
+router.post('/createuser',
+body('email',).isEmail(),
+body('name','Invalid Name').isLength({min:3}),
+body('password','Invalid Password').isLength({min:5}),
+async (req,res)=>{
+    const err=validationResult(req)
+    if(!err.isEmpty()){
+        return res.status(400).json({err:err.array()})
+    }
     try{
         await user.create({
             name: req.body.name,
--- servefood/Backend/db.js
+++ servefood/Backend/db.js
@@ -1,7 +1,7 @@
 const { default: mongoose } = require('mongoose')
 // const mongoose=require('mongoose')
-const mongoURL='mongodb+srv://Amit:Pathak04@fooddeliveryapp.ydngyl4.mongodb.net/food_details?retryWrites=true&w=majority'
-
+// const mongoURL='mongodb+srv://Amit:Pathak04@fooddeliveryapp.ydngyl4.mongodb.net/food_details?retryWrites=true&w=majority'
+const mongoURL='mongodb://Amit:Pathak04@ac-37rtecz-shard-00-00.ydngyl4.mongodb.net:27017,ac-37rtecz-shard-00-01.ydngyl4.mongodb.net:27017,ac-37rtecz-shard-00-02.ydngyl4.mongodb.net:27017/food_details?ssl=true&replicaSet=atlas-1h4cqu-shard-0&authSource=admin&retryWrites=true&w=majority'
 const mongoDB=async()=>{
     await mongoose.connect(mongoURL, {useNewUrlParser: true}, async(err,result)=>{
         if(err){
